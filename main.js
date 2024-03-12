4222636dd0f09a52666487c42ac164c8e648c436
Fatima Munir <81692316+fatima142munir@users.noreply.github.com>
2024-03-12 10:16:11 +0500

    Add files via upload
    day 1 challenge

---
+++ main.js
@@ -0,0 +1,10 @@
+//store a name in variable
+var personName = "Fatima munir";
+// print message with variable
+//console.log("Hello "+personName+", would you like to learn some TypeScript today?");
+//print name with all small letters
+//console.log(personName.toLowerCase());
+//print name with all capital letters
+//console.log(personName.toUpperCase());
+//print name with first letter capital
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()); // Shows the name with the First Letter Big
---
+++ main.ts
@@ -0,0 +1,13 @@
+//store a name in variable
+let personName : string = "Fatima munir";
+// print message with variable
+//console.log("Hello "+personName+", would you like to learn some TypeScript today?");
+
+//print name with all small letters
+//console.log(personName.toLowerCase());
+
+//print name with all capital letters
+//console.log(personName.toUpperCase());
+
+//print name with first letter capital
+console.log(personName.charAt(0).toUpperCase() + personName.slice(1).toLowerCase()); 
